32a1b95749eb7e7b4aaa8d776b9a8bfc7c5a2da0
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2024-02-13 11:54:27 +1100

    radial attack added
    adhd trap

---
+++ entities/attacks/radial_attack.gd
@@ -0,0 +1,26 @@
+extends BaseAttack
+class_name RadialAttack
+
+@export var bullet_scene: PackedScene
+@export var speed: int = 500
+@export var bullet_burst_amount: int = 10
+@export var degrees_offset_per_burst = 5
+@export_range(0.01, 2) var fire_interval: float = 0.3
+@export var fire_amount: int = 10
+
+var bullet_config: BulletConfig
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	preview_attack()
+
+func attack():
+	var rotate_offset = 0
+	var angle_interval = (2 * PI) / bullet_burst_amount
+	for j in range(fire_amount):
+		for i in range(bullet_burst_amount):
+			bullet_config = BulletConfig.new()
+			bullet_config.start_velocity = Vector2.UP.rotated((angle_interval * i) + rotate_offset) * speed
+			add_child(bullet_scene.instantiate().setup(bullet_config))
+		await get_tree().create_timer(fire_interval).timeout
+		rotate_offset += degrees_offset_per_burst
