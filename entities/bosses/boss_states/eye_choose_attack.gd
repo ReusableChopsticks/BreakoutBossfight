2fa14e9aa88af4148beafe0f01677c766293d4b8
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2024-02-26 14:30:59 +1100

    iris changes on each attack

--- entities/bosses/boss_states/eye_choose_attack.gd
+++ entities/bosses/boss_states/eye_choose_attack.gd
@@ -5,6 +5,15 @@ var attacks: Array = []
 var available_attacks: Array = []
 var last_attack = null
 
+var attack_to_iris: Dictionary = {
+	"EyeTrackingBulletAttack": 1,
+	"EyeRadialBeamAttack": 2,
+	"EyeShootPlayerAttack": 3,
+	"CryRainAttack": 4,
+	"EyeBurstAttack": 5
+}
+
+@export var iris: Sprite2D
 @export var anim: AnimationPlayer
 
 # todo: combos? chain attacks? perhaps even simultaneous violences?
@@ -14,11 +23,9 @@ func _ready():
 	for child in get_parent().get_children():
 		if child is AttackState:
 			attacks.append(child)
-	available_attacks.append_array(attacks)
-	print(available_attacks)
-	print("################")
 		#else:
 			#printerr("AttackState %s has non-attack child %s" % [name, child.name])
+	available_attacks.append_array(attacks)
 
 func enter():
 	# make sure no attack repeats twice in a row
@@ -27,8 +34,12 @@ func enter():
 		available_attacks.append(last_attack)
 	last_attack = chosen
 	available_attacks.erase(chosen)
-	print(available_attacks)
 
+	anim.play("close_eye")
+	await anim.animation_finished
+	anim.queue("open_eye")
+	iris.frame = attack_to_iris[chosen.name]
+	await get_tree().create_timer(0.5).timeout
 	transitioned.emit(self, chosen.name)
 	
 func exit():
--- entities/bosses/boss_states/eye_idle_state.gd
+++ entities/bosses/boss_states/eye_idle_state.gd
@@ -4,8 +4,13 @@ class_name EyeIdleState
 @export var idle_time: float = 4
 @export var anim: AnimationPlayer
 
+@onready var eye_anim := $"../../Eye/Animations/EyeAnimationPlayer"
 
 func enter():
+	eye_anim.play("close_eye")
+	await eye_anim.animation_finished
+	eye_anim.queue("open_eye")
+	$"../../Eye/Eyeball/Iris".frame = 0
 	anim.play("idle")
 	await get_tree().create_timer(idle_time).timeout
 	transitioned.emit(self, "EyeChooseAttack")
